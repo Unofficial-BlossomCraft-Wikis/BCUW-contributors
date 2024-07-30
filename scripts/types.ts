f86cbe17cdc62e2cd6bd1443215fb77daf81618e
altie122 <77561831+altie122@users.noreply.github.com>
2024-07-29 22:37:40 -0600

    init

---
+++ scripts/types.ts
@@ -0,0 +1,8 @@
+export interface Contributor {
+  avatar_url: string;
+  issues: Record<string, number>;
+  pulls: Record<string, number>;
+  merged_pulls: Record<string, number>;
+  commits: Record<string, number>;
+  reviews: Record<string, number>;
+}
